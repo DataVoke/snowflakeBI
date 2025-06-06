2899edf650fa9f39f52ccd0a410fb282b30e2381
erichamers <erichamers@gmail.com>
2025-06-06 10:48:20 -0300

    timesheet data validation and testing

---
+++ cai_bi/macros/timesheet/timesheet_source_count_match_sage.sql
@@ -0,0 +1,10 @@
+{% test timesheet_source_count_match_sage(model) %}
+    WITH model_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ model }} WHERE SRC_SYS_KEY = 'int'
+    ),
+    source_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ source('sage_intacct', 'timesheet') }} WHERE _fivetran_deleted = false
+    )
+    SELECT 1
+    WHERE (SELECT cnt FROM model_count) != (SELECT cnt FROM source_count)
+{% endtest %}
---
+++ cai_bi/macros/timesheet/timesheet_source_count_match_sf.sql
@@ -0,0 +1,10 @@
+{% test timesheet_source_count_match_sf(model) %}
+    WITH model_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ model }} WHERE SRC_SYS_KEY = 'sfc'
+    ),
+    source_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ source('salesforce', 'pse_timecard_header_c') }} WHERE _fivetran_deleted = false
+    )
+    SELECT 1
+    WHERE (SELECT cnt FROM model_count) != (SELECT cnt FROM source_count)
+{% endtest %}
