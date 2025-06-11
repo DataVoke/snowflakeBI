eaf36af777826bc8f93a9f9be0430dc911c13eda
erichamers <erichamers@gmail.com>
2025-06-11 16:28:24 -0300

    hotfix for tests names

--- cai_bi/macros/tests/timesheetentry/timesheetentry_source_count_match_sage.sql
+++ cai_bi/macros/tests/timesheetentry/timesheetentry_source_count_match_sage.sql
@@ -1,4 +1,4 @@
-{% test timesheet_source_count_match_sage(model) %}
+{% test timesheetentry_source_count_match_sage(model) %}
     WITH model_count AS (
         SELECT COUNT(*) AS cnt FROM {{ model }} WHERE src_sys_key = 'int'
     ),
