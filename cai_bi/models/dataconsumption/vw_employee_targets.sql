84fa5165e01425f363243432f645bd27147a5b14
Neelima Chennupati <nchennupati@phdata.io>
2025-08-13 16:14:00 -0400

    Employee_Targets - added status and progress

--- cai_bi/models/dataconsumption/vw_employee_targets.sql
+++ cai_bi/models/dataconsumption/vw_employee_targets.sql
@@ -2,7 +2,7 @@
     config(
         materialized="view",
         schema="dataconsumption",
-        alias="vw_billed_hours"
+        alias="vw_employee_targets"
     )
 }}
 
@@ -17,5 +17,7 @@ select ukg_employee_number, count(date_group_id) as week_num ,   round(sum(expec
 round(sum(expected_hours),1) as l12m_target, round(l12m_actual/decode(l12m_target,0,1,l12m_target) * 100,1) as l12m_percentage from {{ref('fct_timesheet_entry_aggregates')}} where 
 type ='Billable' and date_group_type_id ='W' and to_date(date_group_display_name_1)>=dateadd(month,-12,current_date) group by all
 )
-select ytd.ukg_employee_number,ytd.billable_hours_per_week, ytd.billable_hours_per_year, ytd_average_per_week, ytd_actual,ytd_target, ytd_percentage, l12m_average_per_week, l12m_actual, l12m_target, l12m_percentage
+select ytd.ukg_employee_number,ytd.billable_hours_per_week, ytd.billable_hours_per_year, ytd_average_per_week, ytd_actual,ytd_target, ytd_percentage, l12m_average_per_week, l12m_actual, l12m_target, l12m_percentage,
+case when ytd_percentage >= 100 then 'On Track' else 'Off Track' end ytd_status, case when l12m_percentage >= 100 then 'On Track' else 'Off Track' end l12m_status, 
+ytd_actual / decode(ytd.billable_hours_per_year,0,1,ytd.billable_hours_per_year ) *100 as progress
 from ytd left join last_12_mths on ytd.ukg_employee_number = last_12_mths.ukg_employee_number
