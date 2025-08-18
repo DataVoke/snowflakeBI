7f052974220a3c6013b00c05d58fcb7eabf09fe1
Neelima Chennupati <nchennupati@phdata.io>
2025-08-18 13:23:58 -0400

    vw_timesheet_missing_unapproved

--- cai_bi/models/dataconsumption/vw_timesheet_missing_unapproved.sql
+++ cai_bi/models/dataconsumption/vw_timesheet_missing_unapproved.sql
@@ -5,6 +5,7 @@
         alias="vw_timesheet_missing_unapproved"
     )
 }}
+
 select  e.employee_id, e.display_name, e.display_name_lf, e.supervisor_name, e.key_supervisor,sup.email_address_work as supervisor_email,  e.region_name, e.region_id, e.key_region, e.department_name, e.key_department,
 date(e.dts_last_hire),  e.DTE_SRC_START, e.DTE_SRC_END, e.UTILIZATION_TARGET_HOURS, e.TARGET_BILL_HOURS_WEEK_CURRENT, e.location_name, e.status,
 'Missing' as type , null as project_id, null as project_name, null as project_manager_name, null as key_project_manager, null as email_address_work
@@ -20,9 +21,9 @@ date(ge.dts_last_hire),  ge.DTE_SRC_START, ge.DTE_SRC_END, ge.UTILIZATION_TARGET
 and e.src_sys_key = t.src_sys_key and t.src_sys_key ='sfc'
 left join {{ref('dim_employee')}} ge on ge.employee_id = e.employee_id 
 left join {{ref('dim_employee')}} sup on sup.key = ge.key_supervisor 
-left join (select distinct key_timesheet, key_project from {{ref('fct_timesheet_entry')}} where src_sys_key='sfc' ) te on 
+left join (select distinct key_timesheet, key_project from {{ref('timesheet_entry')}} where src_sys_key='sfc' ) te on 
 te.key_timesheet = t.key 
 left join {{ref('project')}} prj on te.key_project = prj.key  and prj.src_sys_key ='sfc'
 left join {{ref('project')}} int_prj on prj.hash_link = int_prj.hash_link and int_prj.src_sys_key ='int'
 left join {{ref('dim_project')}} gold_prj on int_prj.key = gold_prj.key
-where e.src_sys_key ='sfc' and t.hash_key_employee is not null and t.status <> 'Approved'  ; 
+where e.src_sys_key ='sfc' and t.hash_key_employee is not null and t.status <> 'Approved' 
