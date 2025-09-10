611228c6c91c38002807a4e17bb4cf1c9aaa4b87
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2025-09-10 13:04:49 -0400

    update internal logic 2

--- cai_bi/models/dataconsumption/ref_reporting_groups_locations.sql
+++ cai_bi/models/dataconsumption/ref_reporting_groups_locations.sql
@@ -130,27 +130,6 @@ practice_internal as (
         from {{ source('portal', 'reporting_groups_locations') }} l
         left join {{ source('portal', 'reporting_groups') }} r on l.group_id=r.id
         left join {{ source('portal', 'locations') }} loc on l.location_id = loc.id
-            select 
-                concat(l.id,'|') as id,
-                r.id as group_id, 
-                r.parent_id as parent_group_id,
-                l.id as location_id, 
-                r.display_name as group_name, 
-                r.start_date as start_date,
-                r.end_date as end_date,
-                l.record_id as key_location, 
-                loc.id as por_location_id, 
-                loc.salesforce_id as sfc_location_id, 
-                loc.intacct_id as int_location_id, 
-                '' as key_practice, 
-                '' as por_practice_id, 
-                '' as sfc_practice_id, 
-                '' as int_practice_id,
-                iff(current_date>=r.start_date and current_date<r.end_date,true, false) as active,
-                r.sort
-        from prod_bi_dw.cai_portal.reporting_groups_locations l
-        left join prod_bi_dw.cai_portal.reporting_groups r on l.group_id=r.id
-        left join prod_bi_dw.cai_portal.locations loc on l.location_id = loc.id
             where r.parent_id = 'I004' or r.id= 'I004' and r.visible = true
 )
 
