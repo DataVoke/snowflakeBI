b14a4460a0b30d41e539f2d848c9e1e5ab1334d3
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2025-09-29 13:23:17 -0400

    added gold model for proposal line items

---
+++ cai_bi/macros/tests/dim_sales_proposal_liine_item/dim_sales_proposal_line_item_row_count_matches_sources.sql
@@ -0,0 +1,15 @@
+{% test dim_sales_proposal_line_item_row_count_matches_sources(model, source) %}
+    with
+        dim_sales_proposal_line_item_source as (select * from {{ source }} ),
+    model_count as (
+        select count(*) as cnt from {{ model }}
+    ),
+    source_count as (
+        select 
+            count(*) as cnt 
+        from dim_sales_proposal_line_item_source sfc
+        where sfc.src_sys_key = 'sfc'
+    )
+    select 1
+    where (select cnt from model_count) != (select cnt from source_count)
+{% endtest %}
