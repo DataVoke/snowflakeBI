28f532cbea4d31d90d567ebdd53138fc8f6171c8
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2025-11-10 13:39:49 -0500

    add actual bill information

--- cai_bi/models/dataconsumption/report/rpt_labor_budgeting.sql
+++ cai_bi/models/dataconsumption/report/rpt_labor_budgeting.sql
@@ -39,6 +39,58 @@ with
         from fx_rates_timeseries as cc
         where frm_curr in (select currency from currencies_active)
         and to_curr in (select currency from currencies_active)
+    ),
+    billable_time_aggregate_this_year as (
+        select key_employee, 
+            currency_employee,
+            avg(hours) as avg_hours, 
+            sum(hours) as total_hours, 
+            avg(amount_employee) as avg_amount_entity, 
+            sum(amount_employee) as total_amount_entity,
+            avg(amount_usd) as avg_amount_usd, 
+            sum(amount_usd) as total_amount_usd
+        from {{ ref('dim_timesheet_entry_aggregates') }} 
+        where year = year(current_timestamp()) and date_group_type_id='W' and type = 'Billable' and DATE_VALUE <= WEEKOFYEAR(current_timestamp())-1
+        group by all
+    ),
+    total_time_aggregate_this_year as (
+        select key_employee, 
+            currency_employee,
+            avg(hours) as avg_hours, 
+            sum(hours) as total_hours, 
+            avg(amount_employee) as avg_amount_entity, 
+            sum(amount_employee) as total_amount_entity,
+            avg(amount_usd) as avg_amount_usd, 
+            sum(amount_usd) as total_amount_usd
+        from {{ ref('dim_timesheet_entry_aggregates') }} 
+        where year = year(current_timestamp()) and date_group_type_id='W' and type = 'Total' and DATE_VALUE <= WEEKOFYEAR(current_timestamp())-1
+        group by all
+    ),
+    billable_time_aggregate_previous_year as (
+        select key_employee, 
+            currency_employee,
+            avg(hours) as avg_hours, 
+            sum(hours) as total_hours, 
+            avg(amount_employee) as avg_amount_entity, 
+            sum(amount_employee) as total_amount_entity,
+            avg(amount_usd) as avg_amount_usd, 
+            sum(amount_usd) as total_amount_usd
+        from {{ ref('dim_timesheet_entry_aggregates') }} 
+        where year = year(current_timestamp())-1 and date_group_type_id='W' and type = 'Billable'
+        group by all
+    ),
+    total_time_aggregate_previous_year as (
+        select key_employee, 
+            currency_employee,
+            avg(hours) as avg_hours, 
+            sum(hours) as total_hours, 
+            avg(amount_employee) as avg_amount_entity, 
+            sum(amount_employee) as total_amount_entity,
+            avg(amount_usd) as avg_amount_usd, 
+            sum(amount_usd) as total_amount_usd
+        from {{ ref('dim_timesheet_entry_aggregates') }}  
+        where year = year(current_timestamp())-1 and date_group_type_id='W' and type = 'Total'
+        group by all
     )
     
 select 
@@ -78,9 +130,9 @@ select
     ifnull(gold_emp.other_rate_3,0) as other_rate_3_original,
     ifnull(gold_emp.other_rate_4,0) as other_rate_4_original,
     ifnull(forecast_cc_fx_org_to_employee.fx_rate_mul,1) as forecast_conversion_rate_original,
-    ifnull(por_forecast.bill_rate,0) as bill_rate_original,
+    ifnull(por_forecast.bill_rate,0) as plan_bill_rate_original,
+    ifnull(por_forecast.plan_bill_amount_week,0) * forecast_conversion_rate_original as plan_bill_amount_week_original,
     ifnull(por_forecast.plan_bill_amount_year,0) * forecast_conversion_rate_original as plan_bill_amount_year_original,
-    
 
     -- USD Employee Values
     'USD' as currency_id_usd, 
@@ -93,9 +145,15 @@ select
     ifnull(gold_emp.other_rate_3,0) * salary_conversion_rate_usd as other_rate_3_usd,
     ifnull(gold_emp.other_rate_4,0) * salary_conversion_rate_usd as other_rate_4_usd,
     ifnull(forecast_cc_fx_org_to_usd.fx_rate_mul,1) as forecast_conversion_rate_usd,
-    ifnull(por_forecast.bill_rate,0) * forecast_conversion_rate_usd as bill_rate_usd,
+    ifnull(por_forecast.bill_rate,0) * forecast_conversion_rate_usd as plan_bill_rate_usd,
+    ifnull(por_forecast.plan_bill_amount_week,0) * forecast_conversion_rate_usd as plan_bill_amount_week_usd,
     ifnull(por_forecast.plan_bill_amount_year,0) * forecast_conversion_rate_usd as plan_bill_amount_year_usd,
-
+    ifnull(bill_this_year.avg_amount_usd, 0) as actual_bill_amount_week_usd,
+    ifnull(bill_previous_year.avg_amount_usd, 0) as actual_bill_amount_week_usd_previous,
+    ifnull(bill_this_year.total_amount_usd, 0) as actual_bill_amount_year_usd,
+    ifnull(bill_previous_year.total_amount_usd, 0) as actual_bill_amount_year_usd_previous,
+    
+    
     -- Entity Employee Values
     upper(por_entities.currency_id) as currency_id_entity,
     ifnull(gold_cc_fx_org_to_entity.fx_rate_mul,1) as salary_conversion_rate_entity,
@@ -107,15 +165,32 @@ select
     ifnull(gold_emp.other_rate_3,0) * salary_conversion_rate_entity as other_rate_3_entity,
     ifnull(gold_emp.other_rate_4,0) * salary_conversion_rate_entity as other_rate_4_entity,
     ifnull(forecast_cc_fx_org_to_entity.fx_rate_mul,1) as forecast_conversion_rate_entity,
-    ifnull(por_forecast.bill_rate,0) * forecast_conversion_rate_entity as bill_rate_entity,
+    ifnull(por_forecast.bill_rate,0) * forecast_conversion_rate_entity as plan_bill_rate_entity,
+    ifnull(por_forecast.plan_bill_amount_week,0) * forecast_conversion_rate_entity as plan_bill_amount_week_entity,
     ifnull(por_forecast.plan_bill_amount_year,0) * forecast_conversion_rate_entity as plan_bill_amount_year_entity,
-
+    ifnull(bill_this_year.avg_amount_entity, 0) as actual_bill_amount_week_entity,
+    ifnull(bill_previous_year.avg_amount_entity, 0) as actual_bill_amount_week_entity_previous,
+    ifnull(bill_this_year.total_amount_entity, 0) as actual_bill_amount_year_entity,
+    ifnull(bill_previous_year.total_amount_entity, 0) as actual_bill_amount_year_entity_previous,
+    
+    
+    
     //Forecast information
     upper(por_forecast.forecast_currency_id) as currency_id_forecast,
     ifnull(por_forecast.plan_hours_year,0) as plan_hours_year,
     ifnull(por_entities.entity_benefit_rate,0) as benefit_rate,
     gold_emp.status as employee_status,
-    por_forecast.portal_employee_type as employee_type    
+    por_forecast.portal_employee_type as employee_type,
+
+    // Bill Information
+    ifnull(bill_this_year.avg_hours, 0) as actual_bill_hours_week,
+    ifnull(bill_previous_year.avg_hours, 0) as actual_bill_hours_week_previous,
+    ifnull(total_this_year.avg_hours, 0) as actual_total_hours_week,
+    ifnull(total_previous_year.avg_hours, 0) as actual_total_hours_week_previous,
+    ifnull(bill_this_year.total_hours, 0) as actual_total_bill_hours,
+    ifnull(bill_previous_year.total_hours, 0) as actual_total_bill_hours_previous,
+    ifnull(total_this_year.total_hours, 0) as actual_total_hours,
+    ifnull(total_previous_year.total_hours, 0) as actual_total_hours_previous
 from gold_employees gold_emp
 left join portal_entites por_entities on gold_emp.key_entity = por_entities.record_id
 left join portal_departments por_departments on gold_emp.key_department = por_departments.record_id
@@ -147,3 +222,7 @@ left join currency_conversion as forecast_cc_fx_org_to_entity on (
                                                                 and upper(por_entities.currency_id) = forecast_cc_fx_org_to_entity.to_curr
                                                                 and forecast_cc_fx_org_to_entity.date = TO_CHAR(DATE_TRUNC('MONTH', CURRENT_DATE())-1, 'YYYY-MM-DD') 
                                                             )
+left join billable_time_aggregate_this_year bill_this_year on (gold_emp.key = bill_this_year.key_employee)
+left join total_time_aggregate_this_year total_this_year on (gold_emp.key = total_this_year.key_employee)
+left join billable_time_aggregate_previous_year bill_previous_year on (gold_emp.key = bill_previous_year.key_employee)
+left join total_time_aggregate_previous_year total_previous_year on (gold_emp.key = total_previous_year.key_employee)
