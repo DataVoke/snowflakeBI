00f3c8269dd4afcc42ff21dbc8a46b39455d458d
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2025-12-04 13:29:01 -0500

    added distinct to in select

--- cai_bi/models/dataconsumption/views/vw_employee_pay_history.sql
+++ cai_bi/models/dataconsumption/views/vw_employee_pay_history.sql
@@ -20,7 +20,7 @@ with
     compensation as (
         select 'Compensation' as src, *
         from {{ref('dim_employee_compensation')}}
-        where key_employee not in (select key_employee from job_history)
+        where key_employee not in (select distinct key_employee from job_history)
     ),
     employee as (
         select * from {{ref('dim_employee')}}
