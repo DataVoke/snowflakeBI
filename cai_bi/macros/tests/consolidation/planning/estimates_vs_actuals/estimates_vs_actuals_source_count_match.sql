44778b17ef6d4643de9afd794921e13f30f069ce
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2026-01-19 12:39:42 -0500

    [SNOW-347] - EVA

---
+++ cai_bi/macros/tests/consolidation/planning/estimates_vs_actuals/estimates_vs_actuals_source_count_match.sql
@@ -0,0 +1,10 @@
+{% test estimates_vs_actuals_source_count_match(model) %}
+    WITH model_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ model }} WHERE src_sys_key = 'sfc'
+    ),
+    source_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ source('salesforce', 'pse_est_vs_actuals_c') }} WHERE _fivetran_deleted = false
+    )
+    SELECT 1
+    WHERE (SELECT cnt FROM model_count) != (SELECT cnt FROM source_count)
+{% endtest %}
