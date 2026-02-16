135275cd813cafe9eb271be9c8f3004d5d4ffb3a
Evan Gutzwiller <Evan.Gutzwiller@cagents.com>
2026-02-16 13:02:57 -0500

    [SNOW-371] consolidation/invoice_item

---
+++ cai_bi/macros/tests/consolidation/financial/invoice_item/invoice_item_source_count_match.sql
@@ -0,0 +1,10 @@
+{% test invoice_item_source_count_match(model) %}
+    WITH model_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ model }} WHERE SRC_SYS_KEY = 'int'
+    ),
+    source_count AS (
+        SELECT COUNT(*) AS cnt FROM {{ source('sage_intacct', 'ar_invoice_item') }} where _fivetran_deleted=false
+    )
+    SELECT 1
+    WHERE (SELECT cnt FROM model_count) != (SELECT cnt FROM source_count)
+{% endtest %}
